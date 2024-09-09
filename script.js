d8c8945469fad9078da4b6f34391f7cdbcaef6aa
Farid Rahman <faridzrahman19@gmail.com>
2024-09-10 00:03:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ========================================== typing animation ========================================== */
+var typed = new Typed('.typing', {
+    strings:["","Web Developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
